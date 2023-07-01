22ebd96a5c7177b19719cf064322d0fabc2aeb2e
Sven Lüpke <luepke@in.tum.de>
2023-07-01 15:48:47 +0200

    Fix class imblance in miriad

--- miriad.py
+++ miriad.py
@@ -1,11 +1,12 @@
 from torch.utils.data import Dataset, DataLoader
 import os
 import nibabel as nib
+import random
 
 
 class MiriadDataset(Dataset):
 
-    def __init__(self, root : str) -> None:
+    def __init__(self, root : str, train : bool) -> None:
         super().__init__()
 
         self._file_list = []
@@ -14,6 +15,23 @@ class MiriadDataset(Dataset):
             for file in files:
                 if file.endswith(file_format):
                     self._file_list.append(os.path.join(current_root, file))
+
+        reduced_file_list = []
+        ad_count = 0
+        hc_count = 0
+        for file_name in self._file_list:
+            if "AD" in file_name and ad_count < 200:
+                reduced_file_list.append(file_name)
+                ad_count += 1
+            elif "HC" in file_name and hc_count < 200:
+                reduced_file_list.append(file_name)
+                hc_count += 1
+        self._file_list = reduced_file_list
+        random.shuffle(self._file_list)
+        if train:
+            self._file_list = self._file_list[: int(0.8 * len(self._file_list))]
+        else:
+            self._file_list = self._file_list[-int(0.2 * len(self._file_list)) :]
 
     def __len__(self):
         return len(self._file_list)
@@ -33,3 +51,4 @@ class MiriadDataset(Dataset):
         mri_data = mri.get_fdata()
 
         return mri_data, label
+    
