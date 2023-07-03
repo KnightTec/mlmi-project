9967a0bb7ca1f2eb767892e355368a77c6356d3a
Sven Lüpke <luepke@in.tum.de>
2023-07-03 19:49:38 +0200

    Add small swin tranformer model

--- models.py
+++ models.py
@@ -68,3 +68,30 @@ class SvinViTAlzheimersClassifier(nn.Module):
         x = torch.flatten(x)
         return x
     
+class SvinViTAlzheimersClassifierSmall(nn.Module):
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self._svin_vit = SwinViT(
+            in_chans=1, 
+            embed_dim=8,
+            window_size=(5, 5, 5),
+            patch_size=(2, 2, 2),
+            depths=[2, 2, 2, 2],
+            num_heads=[2, 4, 8, 16],
+            mlp_ratio=2.0,
+            qkv_bias=True,
+            drop_rate=0.0,
+            attn_drop_rate=0.0,
+        )
+        self._global_avg_pool = nn.AdaptiveAvgPool3d((1, 1, 1))
+        self._fc = nn.Linear(128, 1)
+
+    def forward(self, x_in):
+        hidden_states_out = self._svin_vit(x_in, True)[4]
+        x = self._global_avg_pool(hidden_states_out)
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = self._fc(x)
+        x = torch.flatten(x)
+        return x
+    
