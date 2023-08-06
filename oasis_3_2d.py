965e8b2c2aaa3d312622388a99c17dea52a62f7f
Sven Lüpke <luepke@in.tum.de>
2023-08-06 23:56:32 +0200

    Add script to create 2d oasis mr data

---
+++ oasis_3_2d.py
@@ -0,0 +1,37 @@
+"""
+Script that takes convert the MR session 3D data of the OASIS-3 data into a 2D dataset by taking the central axial slice. 
+"""
+
+import sys
+import os
+from pathlib import Path
+import nibabel as nib
+
+def main():
+    input_path = sys.argv[1]
+    output_path = sys.argv[2]
+
+    file_format = ".nii.gz"
+    for current_root, dirs, files in os.walk(input_path):
+        new_root = current_root.replace(input_path, output_path)
+        path = Path(new_root)
+        path.mkdir(parents=True, exist_ok=True)
+        for file in files:
+            if file.endswith(file_format):                
+                file_path = os.path.join(current_root, file)
+
+                img1 = nib.load(file_path)
+                data = img1.get_fdata()
+                affine = img1.affine
+
+                data = data[:,:, data.shape[2] // 2]
+
+                new_image = nib.Nifti1Image(data, affine)
+                new_file_path = os.path.join(new_root, file)
+                nib.save(new_image, new_file_path)
+
+                print(f"Created 2D slice file: {new_file_path}")
+
+
+if __name__ == "__main__":
+    main()
