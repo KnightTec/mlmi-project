dde7557320b14ff2b2a5ff3bd206663e500113b8
Sven Lüpke <luepke@in.tum.de>
2023-08-07 13:59:37 +0200

    Update oasis-3 dataset script to include file paths

--- csv/oasis/create_multimodal_dataset.py
+++ csv/oasis/create_multimodal_dataset.py
@@ -4,6 +4,7 @@ This script construct a multimodal dataset using the following 3 OASIS-3 spreads
     - Subjects
     - ADRC Clinical Data
 The paths to the spreadsheets are given to the script via command line arguments.
+The 4th command line argument should contain the path to the mr session image data
 
 The resulting spreadsheet contains the following columns:
     - MR ID
@@ -12,16 +13,16 @@ The resulting spreadsheet contains the following columns:
     - MMSE (Mini–mental state examination) result
     - CDR (Clinical Dementia Rating); 0 = Cognitively Normal, 0.5 = Mild Symptomatic AD, >1 = Alzheimer's Disease
 '''
-# TODO: add more tabular data from ADRC Clinical Data table
 
 import sys
 import pandas as pd
-
+import os
 
 def main():
     mr_session_csv = sys.argv[1]
     subjects_csv = sys.argv[2]
     adrc_csv = sys.argv[3]
+    mr_sessions_path = sys.argv[4]
 
     mr_session_df = pd.read_csv(mr_session_csv)
     mr_session_df = mr_session_df.iloc[:-2]
@@ -54,8 +55,61 @@ def main():
     merged_df = merged_df[["MR ID", "Scans", "M/F", "Age", "mmse", "cdr"]]
     merged_df.rename(columns={'MR ID': 'MR Session ID', "M/F": "Gender"}, inplace=True)
 
-    print(merged_df)
+    merged_df.reset_index()
 
+    # get image file paths
+    mr_file_paths = {} 
+    i = 0
+    for index, row in merged_df.iterrows():
+        mr_id = row["MR Session ID"]
+        mr_file_paths[mr_id] = {}
+        mr_session_path = os.path.join(mr_sessions_path, mr_id)
+        session_file_paths = {
+                "T1w": [],
+                "T2w": [],
+                "T2star": [],
+                "anigo": [],
+                "FLAIR": [],
+            }
+        for current_root, dirs, files in os.walk(mr_session_path):
+            for file in files:
+                if file.endswith(".nii.gz"):
+                    i += 1
+                    print(i)
+                    for key in session_file_paths.keys():
+                        if key in file:
+                            session_file_paths[key].append(file)
+        
+        for key in session_file_paths.keys():
+            file_list = session_file_paths[key]
+            if len(file_list) == 0:
+                session_file_paths[key] = ""
+                continue
+            selected_file = sorted(session_file_paths[key])[-1]
+            session_file_paths[key] = os.path.join(current_root, selected_file).replace(mr_sessions_path, "")
+        
+        mr_file_paths[mr_id] = session_file_paths
+
+    t1w_column = []
+    t2w_column = []
+    t2_star_column = []
+    flair_column = []
+    tof_mra_column = []
+    for mr_session_id in merged_df["MR Session ID"]:
+        paths = mr_file_paths[mr_session_id]
+        t1w_column.append(paths["T1w"])
+        t2w_column.append(paths["T2w"])
+        t2_star_column.append(paths["T2star"])
+        flair_column.append(paths["FLAIR"])
+        tof_mra_column.append(paths["anigo"])
+
+    merged_df["MR T1w"] = t1w_column
+    merged_df["MR T2w"] = t2w_column
+    merged_df["MR T2*"] = t2_star_column
+    merged_df["MR FLAIR"] = flair_column
+    merged_df["MR TOF-MRA"] = tof_mra_column
+
+    print(merged_df)
     merged_df.to_csv("oasis_3_mulitmodal_full.csv", decimal=".", sep=";", index=False, float_format='%.1f')
 
 
