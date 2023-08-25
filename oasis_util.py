7e8b894f514a369a4bf5c2ce2e78f2cbc13f3ce4
Sven Lüpke <luepke@in.tum.de>
2023-08-25 15:14:11 +0200

    Update preprocessing script

---
+++ oasis_util.py
@@ -0,0 +1,49 @@
+import os
+import nibabel as nib
+import matplotlib.pyplot as plt
+import numpy as np
+
+def visualize_structural_mri_session(session_directory):
+    file_format = ".nii.gz"
+
+    modalities = []
+    shapes = []
+    imgs = []
+    for current_root, dirs, files in os.walk(session_directory):
+        for file in files:
+            if file.endswith(file_format):
+                nii_data = nib.load(os.path.join(current_root, file))
+
+                image_data = nii_data.get_fdata()
+                data_shape = image_data.shape
+                print(file)
+
+                half = int(image_data.shape[2] / 3 * 1.5)
+                mri_sample_slice = image_data.astype(np.float32)[:, :, half]
+                imgs.append(mri_sample_slice)
+
+                modality = ""
+                if "T1w" in file:
+                    modality = "T1w"
+                elif "TSE_T2w" in file:
+                    modality = "TSE-T2w"
+                elif "T2w" in file:
+                    modality = "T2w"
+                elif "T2star" in file:
+                    modality = "T2*"
+                elif "FLAIR" in file:
+                    modality = "FLAIR"
+                elif "angio" in file:
+                    modality = "TOF MRA"
+                
+                modalities.append(modality)
+                shapes.append(data_shape)
+
+    fig, ax = plt.subplots(nrows=1, ncols=len(modalities), figsize=(15, 15))
+    for i, img in enumerate(imgs):
+        ax[i].imshow(img.T, cmap='gray')
+        ax[i].axis('off')
+        ax[i].set_title(f"{modalities[i]} {shapes[i]}")  # Set title for each modality image
+
+    plt.tight_layout()            
+    plt.show()
