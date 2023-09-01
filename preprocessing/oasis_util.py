eac0978fbe6fc72b004343ebac78e8930e124a0f
Sven Lüpke <luepke@in.tum.de>
2023-09-01 09:14:52 +0200

    Further optimize preprocessing script

--- preprocessing/oasis_util.py
+++ preprocessing/oasis_util.py
@@ -52,3 +52,33 @@ def visualize_structural_mri_session(session_directory):
 
     plt.tight_layout()            
     plt.show()
+
+def visualize_axial_slice(file_path, fraction=0.5):
+    """
+    Visualize the central axial slice of a .nii file.
+    
+    Parameters:
+        file_path (str): Path to the .nii file
+    """
+    
+    # Load the .nii file
+    img = nib.load(file_path)
+    
+    # Get the image data as a numpy array
+    img_data = img.get_fdata()
+    img_data = img_data[:,:,:,0]
+    
+    # Determine the index of the central slice along the axial axis (axis 2)
+    central_slice_idx = int(img_data.shape[2] * fraction)
+    
+    # Extract the central axial slice
+    central_slice = img_data[:, :, central_slice_idx]
+    
+    # Display the central slice
+    plt.imshow(central_slice.T, cmap="gray", origin="lower")
+    plt.title(f"SRI-24 T1 Template {img_data.shape}")
+    plt.axis("off")
+    plt.show()
+
+# Example usage
+# visualize_central_axial_slice("path/to/your/file.nii")
---
+++ preprocessing/preproc_oasis_1_orient.py
@@ -0,0 +1,104 @@
+import os
+import sys
+import csv
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, as_completed
+import functools
+from monai.transforms import Compose, LoadImaged, Orientationd, SaveImage, EnsureChannelFirstd
+import asyncio
+from tqdm.asyncio import tqdm_asyncio
+import multiprocessing
+import time
+
+# Global ThreadPoolExecutor
+thread_executor = ThreadPoolExecutor(max_workers=8) 
+
+keys = ["image"]
+
+async def async_load_image(keys, item):
+    loop = asyncio.get_running_loop()
+    return await loop.run_in_executor(thread_executor, LoadImaged(keys, image_only=False), item)
+
+async def async_save_image(saver, image, meta_data):
+    loop = asyncio.get_running_loop()
+    await loop.run_in_executor(thread_executor, saver, image, meta_data)
+
+async def process_mr_session(input_session_path, output_session_path):
+    images = []
+    for root, _, files in os.walk(input_session_path):
+        for file in files:
+            if file.lower().endswith(('.nii', '.nii.gz')):
+                images.append(os.path.join(root, file))
+
+    data = [{"image": img,} for img in images]
+
+    image_saver = SaveImage(output_dir=output_session_path, output_postfix="LPS",
+                             output_ext=".nii.gz", resample=False, print_log=False, separate_folder=False)
+
+    transform_pipeline = Compose([
+        EnsureChannelFirstd(keys),
+        Orientationd(keys, axcodes="LPS"),
+    ])
+
+    loop = asyncio.get_event_loop()
+
+    for item in data:
+        loaded_data = await async_load_image(keys, item)
+        transformed = transform_pipeline(loaded_data)
+        if len(transformed["image"].shape) != 4:
+            print(f'Skipping scan {transformed["image_meta_dict"]["filename_or_obj"]} because it does not have 3 dimensions')
+            continue
+        await async_save_image(image_saver, transformed["image"][0, :, :, :], transformed["image_meta_dict"])
+
+def run_mr_sessions_batch(batch_sessions, mr_sessions_path, out_path):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    async def batch_main():
+        tasks = []
+        for item in batch_sessions:
+            session_path = os.path.join(mr_sessions_path, item)
+            out_session_path = os.path.join(out_path, item)
+            os.makedirs(out_session_path, exist_ok=True)
+            tasks.append(process_mr_session(session_path, out_session_path))
+        await asyncio.gather(*tasks)
+
+    loop.run_until_complete(batch_main())
+
+def chunk_sessions(session_ids, chunk_size):
+    for i in range(0, len(session_ids), chunk_size):
+        yield session_ids[i:i + chunk_size]
+
+def main():
+    mr_sessions_path = sys.argv[1]
+    mr_session_ids = sys.argv[2]
+    out_path = sys.argv[3]
+
+    session_ids = []
+    with open(mr_session_ids, newline='') as csvfile:
+        csv_content = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in csv_content:
+            session_ids.append(row[0])
+
+    with ProcessPoolExecutor(max_workers=1) as executor, tqdm(total=len(session_ids)) as pbar:
+        futures = {executor.submit(run_mr_sessions_batch, batch, mr_sessions_path, out_path): batch
+                   for batch in chunk_sessions(session_ids, chunk_size=8)}
+        
+        for future in as_completed(futures):
+            batch = futures[future]
+            try:
+                future.result()
+            except Exception as exc:
+                print(f"A batch generated an exception: {exc}")
+            pbar.update(len(batch))
+
+if __name__ == "__main__":
+    # get the start time
+    st = time.time()
+    # run main
+    main()
+    # get the end time
+    et = time.time()
+    # get the execution time
+    elapsed_time = et - st
+    print('Execution time:', elapsed_time, 'seconds')
